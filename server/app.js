9fca3b2fca95ff32ed3a58b60d7f249f35b13d08
Muhamad Nabila Gozali <bilal@Muhamads-MacBook-Pro.local>
2021-08-09 16:15:29 +0700

    change database

--- server/app.js
+++ server/app.js
@@ -5,7 +5,8 @@ const route = require('./routes')
 const app = express()
 const port = 3000
 
-let database = 'mongodb://localhost:27017/tingwedapse'
+// let database = 'mongodb://localhost:27017/tingwedapse'
+let database = 'mongodb+srv://root:latoelaler87@cluster0.qtp0t.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(database, { useNewUrlParser: true}, function(err){
     if(err) console.log('connection error')
     else console.log('mongoose is connected')
