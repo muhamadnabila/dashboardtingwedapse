4838f717aaa7a606f6e0a41b079b54833fb67d9f
Muhamad Nabila Gozali <bilal@Muhamads-MacBook-Pro.local>
2021-08-09 16:46:16 +0700

    fix password db

--- server/app.js
+++ server/app.js
@@ -3,12 +3,12 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const route = require('./routes')
 const app = express()
-const port = 5000
+const port = 3000
 
 // let database = 'mongodb://localhost:27017/tingwedapse'
-let database = 'mongodb+srv://root:latoelaler87@cluster0.qtp0t.gcp.mongodb.net/tingwedapse?retryWrites=true&w=majority'
-mongoose.connect(database, { useNewUrlParser: true}, function(err){
-    if(err) console.log('connection error')
+let database = 'mongodb+srv://root:root@cluster0.qtp0t.gcp.mongodb.net/tingwedapse?retryWrites=true&w=majority'
+mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true }, function(err){
+    if(err) console.log(err,'connection error')
     else console.log('mongoose is connected')
 })
 mongoose.set('useFindAndModify', false)
